c215774ee2c3cb14fbcd6ca07d5a90e9eee3d2b1
kenadil <180113017@stu.sdu.edu.kz>
2020-07-21 15:55:31 +0600

    Firebase connected

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,23 @@
 import Head from "next/head";
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_pUudH2nhkbzYYiqCPIY1Sdb-TYZQIrg",
+  authDomain: "adilk-efc66.firebaseapp.com",
+  databaseURL: "https://adilk-efc66.firebaseio.com",
+  projectId: "adilk-efc66",
+  storageBucket: "adilk-efc66.appspot.com",
+  messagingSenderId: "954892485083",
+  appId: "1:954892485083:web:73cfe3cfe668f3354a67fe",
+  measurementId: "G-KS56TGG8PP"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 export default function Home() {
+  const db = firebase.database();
+
+  console.log(db);
   return (
     <div className="container">
       <Head>
