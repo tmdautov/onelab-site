29e616cd984a668b8531be90799b22a2ccc3c159
Adil Kenzhebulatov <49553747+kenadil@users.noreply.github.com>
2020-07-23 15:21:37 +0600

    Update layout.tsx

--- pages/components/layout.tsx
+++ pages/components/layout.tsx
@@ -1,8 +1,4 @@
 const style = {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> f1ef61c696fee833789f38d02af2e1d630f3bac4
     width: "960px",
     margin: "0 auto",
   };
@@ -11,19 +7,4 @@ const style = {
     return <div style={style}>{children}</div>;
   }
   
-<<<<<<< HEAD
   export default Layout;
-=======
-  width: "960px",
-  margin: "0 auto",
-};
-
-function Layout({ children }) {
-  return <div style={style}>{children}</div>;
-}
-
-export default Layout;
->>>>>>> origin
-=======
-  export default Layout;
->>>>>>> f1ef61c696fee833789f38d02af2e1d630f3bac4
