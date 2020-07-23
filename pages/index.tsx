1521ecd3091bf1e78bdfd9d211f1a9a9028a4f32
Adil Kenzhebulatov <49553747+kenadil@users.noreply.github.com>
2020-07-23 15:21:11 +0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,15 @@
 import Layout from "./components/layout";
 import Header from "./components/header";
 import Footer from "./components/footer";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { TestComponent } from "./components/TestComponent";
-=======
->>>>>>> origin
-=======
-import { TestComponent } from "./components/TestComponent";
->>>>>>> f1ef61c696fee833789f38d02af2e1d630f3bac4
 
 export default function Home() {
   return (
     <Layout>
       <Header />
-<<<<<<< HEAD
-<<<<<<< HEAD
-	    <p>This is homepage</p>
-      <Footer />
-      <TestComponent />
-=======
 	<p>This is homepage</p>
       <Footer />
->>>>>>> origin
-=======
-	    <p>This is homepage</p>
-      <Footer />
       <TestComponent />
->>>>>>> f1ef61c696fee833789f38d02af2e1d630f3bac4
     </Layout>
   );
 }
