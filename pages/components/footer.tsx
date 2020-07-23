ce698e3681e145a1cc90e269d8bdce29eb0ec90a
Adil Kenzhebulatov <49553747+kenadil@users.noreply.github.com>
2020-07-23 15:23:02 +0600

    Update footer.tsx

--- pages/components/footer.tsx
+++ pages/components/footer.tsx
@@ -1,19 +1,5 @@
 function Footer() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    return <div>I am a footer</div>;
-  }
-  
-  export default Footer;
-=======
   return <div>I am a footer</div>;
 }
 
 export default Footer;
->>>>>>> origin
-=======
-    return <div>I am a footer</div>;
-  }
-  
-  export default Footer;
->>>>>>> f1ef61c696fee833789f38d02af2e1d630f3bac4
