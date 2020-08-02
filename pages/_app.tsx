24e65baff63332b6b6588d20e51df193a2d11f45
AlphaKennyBudy <mr.adil777@gmail.com>
2020-08-02 17:08:36 +0600

    DONE: Rubik font has been added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,11 +15,24 @@ export default function App({ Component, pageProps }) {
     <React.StrictMode>
       <Provider store={store}>
         <Layout>
-          <style jsx>
-            {" "}
+          <style jsx global>
             {`
-              * {
-                justify-content: center;
+              /* rubik-regular - latin_cyrillic */
+              @font-face {
+                font-family: "Rubik";
+                font-style: normal;
+                font-weight: 400;
+                src: local("Rubik"), local("Rubik-Regular"),
+                  url("../public/assets/fonts/rubik-v9-latin_cyrillic-regular.woff2")
+                    format("woff2"),
+                  /* Chrome 26+, Opera 23+, Firefox 39+ */
+                    url("../public/assets/fonts/rubik-v9-latin_cyrillic-regular.woff")
+                    format("woff"); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+
+              body {
+                font-family: Rubik;
+                font-style: normal;
+                font-weight: normal;
               }
             `}
           </style>
