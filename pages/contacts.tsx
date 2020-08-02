0eb74a6e8d73355c4e90a2595f46c505bf49de36
AlphaKennyBudy <mr.adil777@gmail.com>
2020-08-02 17:09:54 +0600

    DONE: Removed unnecessary pages

--- pages/contacts.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from "../components/Layout";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-
-function Contacts() {
-  return <div>I am Contacts</div>;
-}
-
-export default Contacts;
--- pages/faq.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from "../components/Layout";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-
-function FAQ() {
-  return <div>I am FAQ</div>;
-}
-
-export default FAQ;
