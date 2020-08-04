046d870143e876ce1ec3e2a5fdd4385c0c26f19d
AlphaKennyBudy <mr.adil777@gmail.com>
2020-08-04 16:01:24 +0600

    DONE: Direction typization has been removed [2]

--- pages/directions/[code].tsx
+++ pages/directions/[code].tsx
@@ -1,26 +1,19 @@
 import React from "react";
-import {  DirectionItem } from "../../types";
 import Direction from "../../components/Direction";
 import { directions } from "../../services/placeholder";
 
 export default function Products({ product }) {
-
-    return (
-        <Direction product={product} />
-    )
+  return <Direction product={product} />;
 }
 
 Products.getInitialProps = async (ctx) => {
+  const code = Number(ctx.query.code);
 
-    const code = Number(ctx.query.code);
+  const product = directions.find((product) => {
+    if (product.code === code) return product;
+  });
 
-    const product: DirectionItem = directions.find((product) => {
-        if (product.code === code)
-            return product;
-    })
-
-    return {
-        product
-    }
-
-}
+  return {
+    product,
+  };
+};
