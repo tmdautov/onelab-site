4d2acd19831096dcca7f13e041bd825872665bc8
kenadil <180113017@stu.sdu.edu.kz>
2020-08-05 22:28:15 +0600

    FIX: Small fixes

--- components/QuestionAnswer.tsx
+++ components/QuestionAnswer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import theme from "../styles/theme";
+import { RightOutlined, DownOutlined } from "@ant-design/icons";
 
 const QuestionAnswer = ({question, answer}) => {
     const [hidden, setHidden] = React.useState(true);
@@ -19,45 +20,42 @@ const QuestionAnswer = ({question, answer}) => {
                     font-size: 0.95rem;
                     font-weight: 700;
                 }
-
                 .collapsible:focus {
                     border-left: none;
                     border-right: none;
                 }
-
                 .content {
                     padding: 0 1.17vw;
                     width: 32.727vw;
-                    transition: max-height 0.2s ease-out;
+                    transition: max-height 0.2s ease;
                     background-color: ${theme.colors.white};
+                    max-height: 0;
+                    overflow: hidden;
                 }
-
                 .content p {
                     padding: 5px;
                 }
-
                 .hidden {
                     max-height: 0;
                     overflow: hidden;
                 }
-
                 .active {
                     max-height: 100%;
                 } 
-
                 @media (max-width: 1000px) {
                     .collapsible {
                         width: 100%;
                     }
-
                     .content {
                         width: 96%;
                     }
                 }
             `}
             </style>
-            <button className="collapsible" onClick={() => setHidden(!hidden)}>{question}</button>
-            <div className={!hidden ? "content active" : "content hidden"}>
+            <button className="collapsible" onClick={() => setHidden(!hidden)}>{question} 
+                {!hidden ? <DownOutlined style={{float: "right"}}/> : <RightOutlined style={{float: "right"}} />}
+            </button>
+            <div className={!hidden ? "content active" : "content"}>
                 <p>{answer}</p>
             </div>
         </>
