1dc5f1f010cb49398fe8a11f76816f79e02ba667
AlphaKennyBudy <mr.adil777@gmail.com>
2020-08-06 15:05:03 +0600

    DONE: Removed axios from request.service

--- services/requests.service.ts
+++ services/requests.service.ts
@@ -1,15 +1,15 @@
-import axios from "axios";
+const API_URL = "https://oneshop-back.herokuapp.com";
 
-const baseUrl = "https://oneshop-back.herokuapp.com";
-export const instance = axios.create({
-  baseURL: baseUrl,
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-    "Content-Type": "application/json",
-  },
-});
-
-export const RequestsService = {
-  post: (request) => instance.post("/requests", request),
-};
+export default async function postRequest(fd) {
+  return fetch(`${API_URL}/requests`, {
+    method: "POST",
+    body: fd,
+  }).then(async (response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      const error = await response.json();
+      throw new Error(error.message);
+    }
+  });
+}
