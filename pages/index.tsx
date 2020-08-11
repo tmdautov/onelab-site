6e786f726e8f15d13ef0540ce02a2dfe4ff30bb3
Adil <48881444+AlphaKennyBudy@users.noreply.github.com>
2020-08-12 00:03:08 +0600

    Update index.tsx
    FIXED: Changed import order

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import Banner from '../components/Banner/Banner';
 import Profits from '../components/Profits';
 import DirectionsContainer from '../components/Direction/DirectionsContainer';
@@ -6,7 +8,6 @@ import QA from '../components/QA/QA';
 import AcceptingOrder from '../components/Requirements/AcceptingOrder';
 import Requirements from '../components/Requirements/Requirements';
 import EducationInfo from '../components/Requirements/EducationInfo';
-import Head from 'next/head';
 
 export default function Home() {
   return (
